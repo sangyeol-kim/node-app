490aa05fdd70ab34b55f2c8f049b3bfe3df73209
김상열 <yori@yori-MacBook-Pro.local>
2018-11-14 06:56:40 +0900

    app.js update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
 	res.send('Hello World!');
     });
 
-app.listen(4567, function () {
+app.listen(3000, function () {
 	console.log('Example app listening on port 4567!');
     });
 
