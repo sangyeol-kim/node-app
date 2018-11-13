4058ea12096033f34d999d0ea3d06bb9e302017a
sangyeol-kim <twysg@likelion.org>
2018-11-14 02:21:15 +0900

    ok
    kk

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send('Hello World! 되나??');
+	res.send('Hello World!');
     });
 
 app.listen(4567, function () {
